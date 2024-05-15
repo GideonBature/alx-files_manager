d01d2c37b37691674010aa7563921b40d1ec4d44
GideonBature <infoaboutgideon@gmail.com>
2024-05-15 15:12:10 +0100

    simple api that creates a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,42 +1,36 @@
+import { json } from 'express';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
 const crypto = require('crypto');
-const dbClient = require('../utils/db');
+const mongo = require('mongodb');
 
-class UsersController {
-  static async postNew(req, res) {
+module.exports = new class UsersController {
+  async getUser(req, res) {
     const { email, password } = req.body;
-
-    // Input validation
-    if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
+    const users = await dbClient.users;
+    const user = await users.findOne({ email });
+    if (user) {
+      res.status(400).json({ error: 'Already exist' });
     }
-    if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
-    }
-
-    // Check if the email already exists
-    const existingUser = await dbClient.db.collection('users').findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ error: 'Already exists' });
-    }
-
-    // Hash the password using SHA1
-    const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-
-    // Create new user document
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    // Insert the user into the database
-    const result = await dbClient.db.collection('users').insertOne(newUser);
-
-    // Return the new user with only email and id
-    return res.status(201).json({
-      id: result.insertedId,
-      email: newUser.email,
-    });
+    const hashed = crypto.createHash('SHA1').update(password).digest('hex');
+    const userObj = await users.insertOne({ email, password: hashed });
+    const newUser = { id: userObj.insertedId, email };
+    res.status(201).json(newUser);
   }
-}
 
-module.exports = UsersController;
+  async getMe(req, res) {
+    const token = req.headers['x-token'];
+    const id = await redisClient.get(`auth_${token}`);
+    const objID = new mongo.ObjectID(id);
+    const user = await dbClient.users.findOne({ _id: objID });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.json({ id, email: user.email });
+  }
+}();
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const AppController = require('../controllers/AppController');
-const UsersController = require('../controllers/UsersController');
 
 const router = express.Router();
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
-// Define routes
 router.get('/status', AppController.getStatus);
+
 router.get('/stats', AppController.getStats);
 
-// New route for creating a user
-router.post('/users', UsersController.postNew);
+router.post('/users', UsersController.getUser);
+
+router.get('/connect', AuthController.getConnect);
+
+router.get('/disconnect', AuthController.getDisconnect);
+
+router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postFile);
+
+router.get('/files/:id', FilesController.getShow);
+
+router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;
